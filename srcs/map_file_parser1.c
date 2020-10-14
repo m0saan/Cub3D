a1b38be314dcc05583ece9f0a336572642ce21f8
MoBoustta <moboustta6@gmail.com>
2020-10-14 17:51:23 +0100

    Fix the bug -> uninitiated variables

--- srcs/map_file_parser1.c
+++ srcs/map_file_parser1.c
@@ -14,120 +14,120 @@
 
 int count_lines(char *buff)
 {
-	int i;
-	int num_lines;
+    int i;
+    int num_lines;
 
-	i = 0;
-	num_lines = 0;
-	while (buff[i])
-	{
-		if (buff[i] == '\n')
-			num_lines += 1;
-		i++;
-	}
-	return (num_lines);
+    i = 0;
+    num_lines = 0;
+    while (buff[i])
+    {
+        if (buff[i] == '\n')
+            num_lines += 1;
+        i++;
+    }
+    return (num_lines);
 }
 
 int line_length(char *line)
 {
-	int i;
+    int i;
 
-	i = 0;
-	while (line[i] != '\0' && line[i] != '\n')
-		i++;
-	return (i);
+    i = 0;
+    while (line[i] != '\0' && line[i] != '\n')
+        i++;
+    return (i);
 }
 
 int check_boudded_map(t_struct *data)
 {
-	int i;
-	int j;
+    int i;
+    int j;
 
-	i = 0;
-	j = 0;
-	while (i < 14)
-	{
-		j = 0;
-		while (j < 29)
-		{
-			if (data->map[i][j] == 78)
-			{
-				data->x = i * SQUARE_SIZE;
-				data->y = j * SQUARE_SIZE;
-			}
-			if (data->map[i][0] != 1 || data->map[i][data->l_length - 1] != 1)
-				return (0);
-			if (data->map[0][j] != 1 || data->map[data->n_lines - 1][j] != 1)
-				return (0);
-			j++;
-		}
-		i++;
-	}
-	return (1);
+    i = 0;
+    j = 0;
+    while (i < 14)
+    {
+        j = 0;
+        while (j < 29)
+        {
+            if (data->map[i][j] == 78)
+            {
+                data->x = i * SQUARE_SIZE;
+                data->y = j * SQUARE_SIZE;
+            }
+            if (data->map[i][0] != 1 || data->map[i][data->l_length - 1] != 1)
+                return (0);
+            if (data->map[0][j] != 1 || data->map[data->n_lines - 1][j] != 1)
+                return (0);
+            j++;
+        }
+        i++;
+    }
+    return (1);
 }
 int is_not_valid_element(t_struct *data, char *buff)
 {
-	return buff[data->pos] != '1' && buff[data->pos] != '0' && buff[data->pos] != '2' && buff[data->pos] != 'N' && buff[data->pos] != 'W' && buff[data->pos] != 'E' && buff[data->pos] != 'S';
+    return buff[data->pos] != '1' && buff[data->pos] != '0' && buff[data->pos] != '2' && buff[data->pos] != 'N' && buff[data->pos] != 'W' && buff[data->pos] != 'E' && buff[data->pos] != 'S' && buff[data->pos] != ' ';
 }
 
 int is_player(t_struct *data, char *buff)
 {
-	return buff[data->pos] == 'N' || buff[data->pos] == 'W' || buff[data->pos] == 'E' || buff[data->pos] == 'S';
+    return buff[data->pos] == 'N' || buff[data->pos] == 'W' || buff[data->pos] == 'E' || buff[data->pos] == 'S';
 }
 
 int fill_out_map(t_struct *data, char *buff)
 {
-	printf("I was Here\n");
-	int i;
-	int j;
-	int item;
-	int **tmp;
+    printf("I was Here\n");
+    int i;
+    int j;
+    int item;
+    int **tmp;
 
-	i = -1;
-	data->n_lines = count_lines(&buff[data->pos]);
-	while (buff[data->pos] != '\0' && i++ < data->n_lines - 1)
-	{
-		data->l_length = line_length(&buff[data->pos]);
-		data->map[i] = (int *)malloc(sizeof(int) * data->l_length);
-		buff[data->pos] == '\n' ? (data->pos += 1) : data->pos;
-		j = -1;
-		while (j++ < data->l_length && buff[data->pos] != '\n')
-		{
-			if (is_not_valid_element(data, buff))
-				return (0);
-			if (is_player(data, buff))
-			{
-				init_player(data);
-				data->i = i;
-				data->j = j;
-			}
-			data->map[i][j] = ft_atoi(&buff[data->pos]);
-			printf("%d", data->map[i][j]);
-			data->pos += 1;
-		}
-		printf("\n");
-		data->pos += 1;
-	}
-	//return (!(check_boudded_map(data)) ? 0 : 1);
-	return 0;
+    i = -1;
+    data->n_lines = count_lines(&buff[data->pos]);
+    while (buff[data->pos] != '\0' && i++ < data->n_lines - 1)
+    {
+        data->l_length = line_length(&buff[data->pos]);
+        data->map[i] = (int *)malloc(sizeof(int) * data->l_length);
+        buff[data->pos] == '\n' ? (data->pos += 1) : data->pos;
+        j = -1;
+        while (j++ < data->l_length && buff[data->pos] != '\n')
+        {
+            if (is_not_valid_element(data, buff))
+                return (1);
+            if (is_player(data, buff))
+            {
+                data->i = i;
+                data->j = j;
+                init_player(data);
+            }
+            data->map[i][j] = ft_atoi(&buff[data->pos]);
+            printf("%d", data->map[i][j]);
+            data->pos += 1;
+        }
+        printf("\n");
+        data->pos += 1;
+    }
+    //return (!(check_boudded_map(data)) ? 0 : 1);
+    return 0;
 }
 
 void initialize_file_struct(t_struct *data)
 {
-	data->pos = 0;
-	data->get_to_map = 0;
-	data->width = 0;
-	data->height = 0;
-	data->f_red = 0;
-	data->f_green = 0;
-	data->f_blue = 0;
-	data->c_red = 0;
-	data->c_green = 0;
-	data->c_blue = 0;
-	data->map = malloc(sizeof(int) * 30);
-	ft_memset(data->no, 0, 25);
-	ft_memset(data->so, 0, 25);
-	ft_memset(data->ea, 0, 25);
-	ft_memset(data->we, 0, 25);
-	ft_memset(data->sp, 0, 25);
+    data->pos = 0;
+    data->get_to_map = 0;
+    data->width = 0;
+    data->height = 0;
+    data->f_red = 0;
+    data->f_green = 0;
+    data->f_blue = 0;
+    data->c_red = 0;
+    data->c_green = 0;
+    data->c_blue = 0;
+    data->map = malloc(sizeof(int) * 30);
+    ft_memset(data->no, 0, 25);
+    ft_memset(data->so, 0, 25);
+    ft_memset(data->ea, 0, 25);
+    ft_memset(data->we, 0, 25);
+    ft_memset(data->sp, 0, 25);
 }
