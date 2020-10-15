ea0ca6ae8926bc9a0175347f8b99b7ddb5b1db81
MoBoustta <moboustta6@gmail.com>
2020-10-15 13:46:04 +0100

    nothing been added -> commented code

--- srcs/render.c
+++ srcs/render.c
@@ -12,7 +12,7 @@
 
 #include "../include/cube3d.h"
 
-void	render_firt_time(t_struct *data)
+void render_firt_time(t_struct *data)
 {
 	cast_rays(data);
 	render_walls(data);
@@ -21,21 +21,21 @@ void	render_firt_time(t_struct *data)
 	mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img_ptr, 0, 0);
 }
 
-int		update(t_struct *data, uint32_t *buff)
+int update(t_struct *data, uint32_t *buff)
 {
 	mlx_destroy_image(data->mlx_ptr, data->img_ptr);
 	mlx_clear_window(data->mlx_ptr, data->win_ptr);
 	if ((data->img_ptr = mlx_new_image(data->mlx_ptr,
-	data->w_width, data->w_height)) == NULL)
+									   data->w_width, data->w_height)) == NULL)
 		return (TRUE);
 	if ((data->img_data = mlx_get_data_addr(data->img_ptr, &data->bpp,
-	&data->size_line, &data->endian)) == NULL)
+											&data->size_line, &data->endian)) == NULL)
 		return (TRUE);
 	cast_rays(data);
 	move_player(data);
 	render_walls(data);
-	if (data->reset)
-		init_player(data);
+	//if (data->reset)
+	//	init_player(data);
 	if (data->m)
 		mini_map(data);
 	mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img_ptr, 0, 0);
@@ -44,15 +44,13 @@ int		update(t_struct *data, uint32_t *buff)
 	return (FALSE);
 }
 
-int		move_player(t_struct *data)
+int move_player(t_struct *data)
 {
 	data->rotation_angle += data->turn_direction * data->turn_speed;
 	data->move_step = data->walk_direction * data->walk_speed;
 	data->move_step += data->shift ? 10 : 0;
-	data->updated_player_x = data->x
-	+ cos(data->rotation_angle) * data->move_step;
-	data->updated_player_y = data->y
-	+ sin(data->rotation_angle) * data->move_step;
+	data->updated_player_x = data->x + cos(data->rotation_angle) * data->move_step;
+	data->updated_player_y = data->y + sin(data->rotation_angle) * data->move_step;
 	if (!if_wall(data->updated_player_x, data->updated_player_y, data))
 	{
 		data->x = data->updated_player_x;
