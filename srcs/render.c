79e4741f6a8f122c354612eb39f3340621c18dbe
MoBoustta <moboustta6@gmail.com>
2020-10-16 10:07:51 +0100

    remove some unused code

--- srcs/render.c
+++ srcs/render.c
@@ -23,14 +23,14 @@ void render_firt_time(t_struct *data)
 
 int update(t_struct *data)
 {
-	mlx_destroy_image(data->mlx_ptr, data->img_ptr);
-	mlx_clear_window(data->mlx_ptr, data->win_ptr);
-	if ((data->img_ptr = mlx_new_image(data->mlx_ptr,
-									   data->w_width, data->w_height)) == NULL)
-		return (TRUE);
-	if ((data->img_data = mlx_get_data_addr(data->img_ptr, &data->bpp,
-											&data->size_line, &data->endian)) == NULL)
-		return (TRUE);
+	// mlx_destroy_image(data->mlx_ptr, data->img_ptr);
+	// mlx_clear_window(data->mlx_ptr, data->win_ptr);
+	// if ((data->img_ptr = mlx_new_image(data->mlx_ptr,
+	// 								   data->w_width, data->w_height)) == NULL)
+	// 	return (TRUE);
+	// if ((data->img_data = mlx_get_data_addr(data->img_ptr, &data->bpp,
+	// 										&data->size_line, &data->endian)) == NULL)
+	// 	return (TRUE);
 	cast_rays(data);
 	render_walls(data);
 	move_player(data);
