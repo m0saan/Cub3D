1f52d50c83ebb262328ec7eb6f5dfeb3e54a5afe
MoBoustta <moboustta6@gmail.com>
2020-10-17 14:58:35 +0100

    simplify code

--- srcs/sprite.c
+++ srcs/sprite.c
@@ -26,9 +26,10 @@ void draw_sprites(t_struct *data, float x_off, float y_off, int index)
 				continue;
 			id = SPRITE_SIZE * (SPRITE_SIZE * j / size) + (SPRITE_SIZE * i / size);
 			id = id >= (e) ? (e - 1) : id;
-			if (!(data->img_data_texture_sprite[id] == BLACK))
-				ft_draw(data, x_off + i, y_off + j, data->img_data_texture_sprite[id]);
-		}
+            if (data->img_data_texture_sprite[id] == BLACK)
+                continue;
+            ft_draw(data, x_off + i, y_off + j, data->img_data_texture_sprite[id]);
+        }
 	}
 }
 
