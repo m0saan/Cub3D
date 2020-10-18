6dfa5823b391675db2391e38892c1278f3ee0cf7
MoBoustta <moboustta6@gmail.com>
2020-10-18 11:57:19 +0100

    add cmake to project

---
+++ cmake-build-debug/CMakeFiles/TEST.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/moboustt/Desktop/cursus/Cubb/srcs/cube3d_utils.c" "/Users/moboustt/Desktop/cursus/Cubb/cmake-build-debug/CMakeFiles/TEST.dir/srcs/cube3d_utils.c.o"
+  "/Users/moboustt/Desktop/cursus/Cubb/srcs/events.c" "/Users/moboustt/Desktop/cursus/Cubb/cmake-build-debug/CMakeFiles/TEST.dir/srcs/events.c.o"
+  "/Users/moboustt/Desktop/cursus/Cubb/srcs/hud.c" "/Users/moboustt/Desktop/cursus/Cubb/cmake-build-debug/CMakeFiles/TEST.dir/srcs/hud.c.o"
+  "/Users/moboustt/Desktop/cursus/Cubb/srcs/main.c" "/Users/moboustt/Desktop/cursus/Cubb/cmake-build-debug/CMakeFiles/TEST.dir/srcs/main.c.o"
+  "/Users/moboustt/Desktop/cursus/Cubb/srcs/map_file_parser.c" "/Users/moboustt/Desktop/cursus/Cubb/cmake-build-debug/CMakeFiles/TEST.dir/srcs/map_file_parser.c.o"
+  "/Users/moboustt/Desktop/cursus/Cubb/srcs/map_file_parser1.c" "/Users/moboustt/Desktop/cursus/Cubb/cmake-build-debug/CMakeFiles/TEST.dir/srcs/map_file_parser1.c.o"
+  "/Users/moboustt/Desktop/cursus/Cubb/srcs/map_file_parser2.c" "/Users/moboustt/Desktop/cursus/Cubb/cmake-build-debug/CMakeFiles/TEST.dir/srcs/map_file_parser2.c.o"
+  "/Users/moboustt/Desktop/cursus/Cubb/srcs/map_file_parser3.c" "/Users/moboustt/Desktop/cursus/Cubb/cmake-build-debug/CMakeFiles/TEST.dir/srcs/map_file_parser3.c.o"
+  "/Users/moboustt/Desktop/cursus/Cubb/srcs/minimap.c" "/Users/moboustt/Desktop/cursus/Cubb/cmake-build-debug/CMakeFiles/TEST.dir/srcs/minimap.c.o"
+  "/Users/moboustt/Desktop/cursus/Cubb/srcs/ray_casting.c" "/Users/moboustt/Desktop/cursus/Cubb/cmake-build-debug/CMakeFiles/TEST.dir/srcs/ray_casting.c.o"
+  "/Users/moboustt/Desktop/cursus/Cubb/srcs/render.c" "/Users/moboustt/Desktop/cursus/Cubb/cmake-build-debug/CMakeFiles/TEST.dir/srcs/render.c.o"
+  "/Users/moboustt/Desktop/cursus/Cubb/srcs/screenshot.c" "/Users/moboustt/Desktop/cursus/Cubb/cmake-build-debug/CMakeFiles/TEST.dir/srcs/screenshot.c.o"
+  "/Users/moboustt/Desktop/cursus/Cubb/srcs/sprite.c" "/Users/moboustt/Desktop/cursus/Cubb/cmake-build-debug/CMakeFiles/TEST.dir/srcs/sprite.c.o"
+  "/Users/moboustt/Desktop/cursus/Cubb/srcs/utils.c" "/Users/moboustt/Desktop/cursus/Cubb/cmake-build-debug/CMakeFiles/TEST.dir/srcs/utils.c.o"
+  "/Users/moboustt/Desktop/cursus/Cubb/srcs/walls.c" "/Users/moboustt/Desktop/cursus/Cubb/cmake-build-debug/CMakeFiles/TEST.dir/srcs/walls.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/TEST.dir/cmake_clean.cmake
@@ -0,0 +1,24 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TEST.dir/srcs/cube3d_utils.c.o"
+  "CMakeFiles/TEST.dir/srcs/events.c.o"
+  "CMakeFiles/TEST.dir/srcs/hud.c.o"
+  "CMakeFiles/TEST.dir/srcs/main.c.o"
+  "CMakeFiles/TEST.dir/srcs/map_file_parser.c.o"
+  "CMakeFiles/TEST.dir/srcs/map_file_parser1.c.o"
+  "CMakeFiles/TEST.dir/srcs/map_file_parser2.c.o"
+  "CMakeFiles/TEST.dir/srcs/map_file_parser3.c.o"
+  "CMakeFiles/TEST.dir/srcs/minimap.c.o"
+  "CMakeFiles/TEST.dir/srcs/ray_casting.c.o"
+  "CMakeFiles/TEST.dir/srcs/render.c.o"
+  "CMakeFiles/TEST.dir/srcs/screenshot.c.o"
+  "CMakeFiles/TEST.dir/srcs/sprite.c.o"
+  "CMakeFiles/TEST.dir/srcs/utils.c.o"
+  "CMakeFiles/TEST.dir/srcs/walls.c.o"
+  "TEST"
+  "TEST.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/TEST.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
