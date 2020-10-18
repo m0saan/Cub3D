7721131725055aac7631d88e6eee9cebc6dc4cba
MoBoustta <moboustta6@gmail.com>
2020-10-18 09:17:10 +0100

    implement a screenshot as a bmp format

---
+++ Makefile
@@ -0,0 +1,36 @@
+# -*- This is a MakeFile -*- #
+
+NAME = Cub3D
+SRC_PATH = srcs
+SRC_NAMES = *.c
+
+OBJ_PATH = objs
+OBJ_NAME = $(SRC_NAMES:.c=.o)
+
+CC = gcc
+CFLAGS = -Wall -Wextra -Werror -std=c11
+DEBUG = -g
+OPTIMAZATION = -Ofast
+LDLIBS = -lm -framework OpenGL -framework AppKit libmlx.a -fsanitize=address
+
+SRC = $(addprefix $(SRC_PATH)/, $(SRC_NAMES))
+OBJ = $(addprefix $(OBJ_PATH)/, $(OBJ_NAME))
+
+
+all: $(NAME)
+
+$(NAME):
+	@$(CC) $(DEBUG) $(LDLIBS) $(SRC) -o $(NAME);
+
+clean:
+	@rm -rf *.o
+	@echo "Makefile : Cleaning .o files..."
+
+build:
+	@./Cub3D map.cub
+
+fclean: clean
+	@rm -rf $(NAME)
+	@echo "Makefile : Clean .a files..."
+
+re: fclean all
--- srcs/screenshot.c
+++ srcs/screenshot.c
@@ -1,42 +1,87 @@
+
+
 #include "../include/cube3d.h"
 
-void			scrn_data(t_data *data, int x)
+typedef struct s_rgb
 {
-    creatergb()
-    g_color = colorconverter(data->img.data[data->screen.row *
-                                            data->screen.width + data->screen.col]);
-    data->screen.buf[x * data->screen.width_in_bytes
-                   + data->screen.col * 3 + 0] = g_color.b;
-    data->screen.buf[x * data->screen.width_in_bytes
-                   + data->screen.col * 3 + 1] = g_color.g;
-    data->screen.buf[x * data->screen.width_in_bytes
-                   + data->screen.col * 3 + 2] = g_color.r;
+    int r;
+    int g;
+    int b;
+} t_rgb;
+
+struct s_rgb g_rgb;
+
+struct s_rgb color_converter(int hex_value)
+{
+    t_rgb rgb_color;
+
+    rgb_color.r = ((hex_value >> 16) & 0xFF);
+    rgb_color.g = ((hex_value >> 8) & 0xFF);
+    rgb_color.b = ((hex_value)&0xFF);
+    return (rgb_color);
 }
 
-int				screen(t_struct *data)
+int screen(t_struct *data)
 {
-    int				x;
-    unsigned char	header[54];
-    int				i;
+    int x;
+    const char *file_name = "screenshot.bmp";
+    const int LEN = 54;
+    unsigned char header[LEN];
+    int i;
 
     i = 0;
-    while (i < 54)
-        header[i++] = 0;
-    //screen_init(data, header);
-    data->screen.buf = malloc((data->screen.imagesize));
+    ft_memset(header, 0, LEN);
+    screen_init(data, header);
+    data->screen.buf = malloc((data->screen.image_size));
     x = 0;
     data->screen.row = data->screen.height - 1;
     while (data->screen.row-- >= 0)
     {
         data->screen.col = 0;
         while (data->screen.col++ < data->screen.width)
-            //scrn_data(data, x);
+            scrn_data(data, x);
         x++;
     }
-    data->screen.fd = open(S, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
-    write(data->screen.fd, header, 54);
-    write(data->screen.fd, (char*)data->screen.buf, data->screen.imagesize);
+    data->screen.fd = open(file_name, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
+    write(data->screen.fd, header, LEN);
+    write(data->screen.fd, (char *)data->screen.buf, data->screen.image_size);
     close(data->screen.fd);
     free(data->screen.buf);
     return (0);
+}
+
+void scrn_data(t_struct *data, int x)
+{
+    g_rgb = color_converter(data->img_data[data->screen.row *
+                                               data->screen.width +
+                                           data->screen.col]);
+    data->screen.buf[x * data->screen.width_in_bytes + data->screen.col * 3 + 0] = g_rgb.b;
+    data->screen.buf[x * data->screen.width_in_bytes + data->screen.col * 3 + 1] = g_rgb.g;
+    data->screen.buf[x * data->screen.width_in_bytes + data->screen.col * 3 + 2] = g_rgb.r;
+}
+
+void screen_init(t_struct *data, unsigned char *header)
+{
+    data->screen.width = (int32_t)data->w_width;
+    data->screen.height = (int32_t)data->m_height;
+    data->screen.bit_count = 24;
+    data->screen.width_in_bytes = ((data->screen.width *
+                                        data->screen.bit_count +
+                                    31) /
+                                   32) *
+                                  4;
+    data->screen.image_size = data->screen.width_in_bytes * data->screen.height;
+    data->screen.bi_size = 40;
+    data->screen.bf_off_bits = 54;
+    data->screen.file_size = 54 + data->screen.image_size;
+    data->screen.biplanes = 1;
+    ft_memcpy(header, "BM", 2);
+    ft_memcpy(header + 2, &(data->screen.file_size), 4);
+    ft_memcpy(header + 10, &(data->screen.bf_off_bits), 4);
+    ft_memcpy(header + 14, &(data->screen.bi_size), 4);
+    ft_memcpy(header + 18, &(data->screen.width), 4);
+    ft_memcpy(header + 22, &(data->screen.height), 4);
+    ft_memcpy(header + 26, &(data->screen.biplanes), 2);
+    ft_memcpy(header + 28, &(data->screen.bit_count), 2);
+    ft_memcpy(header + 34, &(data->screen.image_size), 4);
 }
