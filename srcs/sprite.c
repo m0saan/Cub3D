3976d5648902dcfca8963c70639fc68421e4bdcf
MoBoustta <moboustta6@gmail.com>
2020-10-20 12:17:17 +0100

    Refactor code -> normed

--- srcs/sprite.c
+++ srcs/sprite.c
@@ -6,48 +6,21 @@
 /*   By: moboustt <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/20 11:26:10 by moboustt          #+#    #+#             */
-/*   Updated: 2020/10/20 11:35:25 by moboustt         ###   ########.fr       */
+/*   Updated: 2020/10/20 12:17:00 by moboustt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cube3d.h"
 
-static void	bubble_sort(t_struct *data)
-{
-	int			i;
-	int			j;
-	t_sprite	tmp;
-
-	i = 0;
-	while (i < data->count_spt)
-	{
-		j = 0;
-		while (j < data->count_spt - i && j + 1 < data->count_spt)
-		{
-			if (data->sprite[j].dis < data->sprite[j + 1].dis)
-			{
-				tmp = data->sprite[j];
-				data->sprite[j] = data->sprite[j + 1];
-				data->sprite[j + 1] = tmp;
-			}
-			j++;
-		}
-		i++;
-	}
-}
-
-void		draw_sprites(t_struct *data, float x_off,
+void	draw_sprites(t_struct *data, float x_off,
 		float y_off, int index)
 {
 	size_t		i;
 	size_t		j;
 	size_t		size;
 	int			id;
-	const int	e = SPRITE_SIZE * SPRITE_SIZE;
 
 	i = 0;
-	j = 0;
-	id = 0;
 	size = data->sprite[index].size;
 	while (i++ < size)
 	{
@@ -60,9 +33,7 @@ void		draw_sprites(t_struct *data, float x_off,
 		{
 			if (y_off + j < 0 || y_off + j > (float)data->w_height)
 				continue;
-			id = SPRITE_SIZE * (SPRITE_SIZE * j / size)
-				+ (SPRITE_SIZE * i / size);
-			id = id >= (e) ? (e - 1) : id;
+			id = get_color_id(i, j, size);
 			if (data->img_data_texture_sprite[id] != BLACK)
 				ft_draw(data, x_off + i, y_off + j,
 						data->img_data_texture_sprite[id]);
@@ -70,7 +41,7 @@ void		draw_sprites(t_struct *data, float x_off,
 	}
 }
 
-void		set_up_sprite(t_struct *data)
+void	set_up_sprite(t_struct *data)
 {
 	float	spt_angle;
 	int		i_spt;
@@ -78,68 +49,41 @@ void		set_up_sprite(t_struct *data)
 
 	i = 0;
 	i_spt = 0;
-
-	while (i < data->count_spt)
-	{
-		data->sprite[i].dis = distance_between_points(data->x,
-				data->y, data->sprite[i].x, data->sprite[i].y);
-		i++;
-	}
+	set_sprite_distance(data, &i);
 	bubble_sort(data);
 	while (i_spt < data->count_spt)
 	{
 		spt_angle = atan2f(data->sprite[i_spt].y - data->y,
 				data->sprite[i_spt].x - data->x);
-		while (spt_angle - data->rotation_angle > M_PI)
-			spt_angle -= 2 * M_PI;
-		while (spt_angle - data->rotation_angle < -M_PI)
-			spt_angle += 2 * M_PI;
-		if (data->w_height > data->w_width)
-			data->sprite[i_spt].size = ((float )data->w_height
-					/ data->sprite[i_spt].dis) * SQUARE_SIZE;
-		else
-			data->sprite[i_spt].size = ((float )data->w_width
-					/ data->sprite[i_spt].dis) * SQUARE_SIZE;
-		data->sprite[i_spt].y_off = (float )data->w_height / 2
-			- data->sprite[i_spt].size / 2;
-		data->sprite[i_spt].x_off = ((DEG(spt_angle)
-					- DEG(data->rotation_angle)) * data->w_width)
-			/ SPRITE_SIZE + (((float )data->w_width / 2)
-					- (data->sprite[i_spt].size / 2));
+		normalize_sprite_angle(data, &spt_angle);
+		normalize_sprite_size(data, &i_spt);
+		data->sprite[i_spt].y_off = calc_sprite_y_offset(data, i_spt);
+		data->sprite[i_spt].x_off = calc_sprite_x_offset(data, i_spt, spt_angle);
 		draw_sprites(data, data->sprite[i_spt].x_off,
 				data->sprite[i_spt].y_off, i_spt);
 		i_spt++;
 	}
 }
 
-void		initialize_sprite(t_struct *data)
+void	initialize_sprite(t_struct *data)
 {
 	int i;
 	int j;
 	int i_spt;
 
-	i = 0;
+	i = -1;
 	j = 0;
 	i_spt = 0;
 	if (data->count_spt == 0)
-		return;
+		return ;
 	if (!(data->sprite = (t_sprite *)malloc(sizeof(t_sprite)
 					* (data->count_spt))))
-		return;
-	while (i < data->n_lines)
+		return ;
+	while (++i < data->n_lines)
 	{
-		j = 0;
-		while (j < g_lines_length[i])
-		{
-			if (data->map[i][j] == '2')
-			{
-				data->sprite[i_spt].x = ((float )j + 0.5f) * SQUARE_SIZE;
-				data->sprite[i_spt].y = ((float )i + 0.5f) * SQUARE_SIZE;
-				i_spt++;
-			}
-			j++;
-		}
-		i++;
+		j = -1;
+		while (++j < g_lines_length[i])
+			if (is_sprite(data->map[i][j]))
+				set_sprite_coordinates(data, &i_spt, i, j);
 	}
 }
-
