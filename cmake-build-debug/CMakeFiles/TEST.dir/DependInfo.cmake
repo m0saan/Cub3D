8f0a9a2219dca857d2f5af07e4983aa122111266
MoBoustta <moboustta6@gmail.com>
2020-10-20 14:36:32 +0100

    cmake

---
+++ cmake-build-debug/CMakeFiles/TEST.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/moboustt/Desktop/VVVVV/srcs/cube3d_utils.c" "/Users/moboustt/Desktop/VVVVV/cmake-build-debug/CMakeFiles/TEST.dir/srcs/cube3d_utils.c.o"
+  "/Users/moboustt/Desktop/VVVVV/srcs/events.c" "/Users/moboustt/Desktop/VVVVV/cmake-build-debug/CMakeFiles/TEST.dir/srcs/events.c.o"
+  "/Users/moboustt/Desktop/VVVVV/srcs/init_data.c" "/Users/moboustt/Desktop/VVVVV/cmake-build-debug/CMakeFiles/TEST.dir/srcs/init_data.c.o"
+  "/Users/moboustt/Desktop/VVVVV/srcs/intersection.c" "/Users/moboustt/Desktop/VVVVV/cmake-build-debug/CMakeFiles/TEST.dir/srcs/intersection.c.o"
+  "/Users/moboustt/Desktop/VVVVV/srcs/main.c" "/Users/moboustt/Desktop/VVVVV/cmake-build-debug/CMakeFiles/TEST.dir/srcs/main.c.o"
+  "/Users/moboustt/Desktop/VVVVV/srcs/map_file_parser.c" "/Users/moboustt/Desktop/VVVVV/cmake-build-debug/CMakeFiles/TEST.dir/srcs/map_file_parser.c.o"
+  "/Users/moboustt/Desktop/VVVVV/srcs/map_file_parser1.c" "/Users/moboustt/Desktop/VVVVV/cmake-build-debug/CMakeFiles/TEST.dir/srcs/map_file_parser1.c.o"
+  "/Users/moboustt/Desktop/VVVVV/srcs/map_file_parser2.c" "/Users/moboustt/Desktop/VVVVV/cmake-build-debug/CMakeFiles/TEST.dir/srcs/map_file_parser2.c.o"
+  "/Users/moboustt/Desktop/VVVVV/srcs/map_file_parser3.c" "/Users/moboustt/Desktop/VVVVV/cmake-build-debug/CMakeFiles/TEST.dir/srcs/map_file_parser3.c.o"
+  "/Users/moboustt/Desktop/VVVVV/srcs/ray_casting.c" "/Users/moboustt/Desktop/VVVVV/cmake-build-debug/CMakeFiles/TEST.dir/srcs/ray_casting.c.o"
+  "/Users/moboustt/Desktop/VVVVV/srcs/render.c" "/Users/moboustt/Desktop/VVVVV/cmake-build-debug/CMakeFiles/TEST.dir/srcs/render.c.o"
+  "/Users/moboustt/Desktop/VVVVV/srcs/screenshot.c" "/Users/moboustt/Desktop/VVVVV/cmake-build-debug/CMakeFiles/TEST.dir/srcs/screenshot.c.o"
+  "/Users/moboustt/Desktop/VVVVV/srcs/sprite.c" "/Users/moboustt/Desktop/VVVVV/cmake-build-debug/CMakeFiles/TEST.dir/srcs/sprite.c.o"
+  "/Users/moboustt/Desktop/VVVVV/srcs/sprite_utils.c" "/Users/moboustt/Desktop/VVVVV/cmake-build-debug/CMakeFiles/TEST.dir/srcs/sprite_utils.c.o"
+  "/Users/moboustt/Desktop/VVVVV/srcs/sprite_utils2.c" "/Users/moboustt/Desktop/VVVVV/cmake-build-debug/CMakeFiles/TEST.dir/srcs/sprite_utils2.c.o"
+  "/Users/moboustt/Desktop/VVVVV/srcs/utils.c" "/Users/moboustt/Desktop/VVVVV/cmake-build-debug/CMakeFiles/TEST.dir/srcs/utils.c.o"
+  "/Users/moboustt/Desktop/VVVVV/srcs/wall_utils.c" "/Users/moboustt/Desktop/VVVVV/cmake-build-debug/CMakeFiles/TEST.dir/srcs/wall_utils.c.o"
+  "/Users/moboustt/Desktop/VVVVV/srcs/walls.c" "/Users/moboustt/Desktop/VVVVV/cmake-build-debug/CMakeFiles/TEST.dir/srcs/walls.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
