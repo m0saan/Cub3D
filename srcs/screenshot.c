e7b150cb326b6376bf0aed8d09de5fd8303121e7
MoBoustta <moboustta6@gmail.com>
2020-10-22 09:47:22 +0100

    update file to new var names

--- srcs/screenshot.c
+++ srcs/screenshot.c
@@ -48,32 +48,38 @@ void	screen_data(t_struct *data, int x)
 	index = data->bitmap.row * data->w_width + data->bitmap.col;
 	index = index < 0 ? index * (-1) : index;
 	g_rgb = color_converter((uint32_t)data->img_data_bmp[index]);
-	data->bitmap.buf[x * data->bitmap.width_in_bytes
+	data->bitmap.buf[x * data->bitmap.width_in_pxl
 		+ data->bitmap.col * 3 + 0] = g_rgb->b;
-	data->bitmap.buf[x * data->bitmap.width_in_bytes
+	data->bitmap.buf[x * data->bitmap.width_in_pxl
 		+ data->bitmap.col * 3 + 1] = g_rgb->g;
-	data->bitmap.buf[x * data->bitmap.width_in_bytes
+	data->bitmap.buf[x * data->bitmap.width_in_pxl
 		+ data->bitmap.col * 3 + 2] = g_rgb->r;
 	free(g_rgb);
 }
 
 void	screen_init(t_struct *data, unsigned char *header)
 {
-	data->bitmap.bit_count = 24;
-	data->bitmap.width_in_bytes = ((data->w_width *
-				data->bitmap.bit_count + 31) / 32) * 4;
-	data->bitmap.image_size = data->bitmap.width_in_bytes * data->w_height;
-	data->bitmap.bi_size = 40;
+    // Insert colors into bmp buffer
+    // copy memory infos to bmp header.
+	data->bitmap.bit_per_pxl = 24;
+	data->bitmap.width_in_pxl = ((data->w_width *
+                                  data->bitmap.bit_per_pxl + 31) / 32) * 4;
+	data->bitmap.image_size = data->bitmap.width_in_pxl * data->w_height;
+	data->bitmap.info_header_size = 40;
 	data->bitmap.bf_off_bits = 54;
 	data->bitmap.file_size = 54 + data->bitmap.image_size;
 	data->bitmap.biplanes = 1;
 	ft_memcpy(header, "BM", 2);
 	ft_memcpy(header + 2, &(data->bitmap.file_size), 4);
 	ft_memcpy(header + 10, &(data->bitmap.bf_off_bits), 4);
-	ft_memcpy(header + 14, &(data->bitmap.bi_size), 4);
+	ft_memcpy(header + 14, &(data->bitmap.info_header_size), 4);
 	ft_memcpy(header + 18, &(data->w_width), 4);
 	ft_memcpy(header + 22, &(data->w_height), 4);
 	ft_memcpy(header + 26, &(data->bitmap.biplanes), 2);
-	ft_memcpy(header + 28, &(data->bitmap.bit_count), 2);
-	ft_memcpy(header + 34, &(data->bitmap.image_size), 4);
+	ft_memcpy(header + 28, &(data->bitmap.bit_per_pxl), 2);
+    //667754236940000054000400001287005640024000000000000000000000000000
+    //667754236940000054000400001287005640010240000000000000000000000000
+    for (int i = 0; i < 54; ++i) {
+        printf("%u", header[i]);
+    }
 }
