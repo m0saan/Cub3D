394e32e065b147c685ae5d3597296e110e93d060
MoBoustta <moboustta6@gmail.com>
2020-10-23 10:17:32 +0100

    implement the is_valid_color function

--- srcs/utils2.c
+++ srcs/utils2.c
@@ -6,7 +6,7 @@
 /*   By: moboustt <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/20 14:54:58 by moboustt          #+#    #+#             */
-/*   Updated: 2020/10/21 10:11:10 by moboustt         ###   ########.fr       */
+/*   Updated: 2020/10/23 10:00:13 by moboustt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,18 @@ int		skip_number(const char *str)
 	i = 0;
 	if (str[i] == ',' || str[i] == ' ')
 		i++;
-	while (str[i] >= 48 && str[i] <= 57 && (str[i] != ' ' || str[i] != ','))
+	while ((str[i] >= 48 && str[i] <= 57) && (str[i] != ' ' || str[i] != ','))
 		i++;
 	return (i);
 }
 
 void	error(char *str)
 {
-	write(1, str, ft_strlen(str));
+    write(1, str, ft_strlen(str));
 	exit(EXIT_FAILURE);
+}
+
+int     is_valid_color(const t_struct *data, int id)
+{
+    return data->sprite_tex_data[id] != BLACK;
 }
