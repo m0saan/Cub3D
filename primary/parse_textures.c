509adf1aa6847c0145ffee0092d4b6bb45cbf2ff
MoBoustta <moboustta6@gmail.com>
2020-11-28 11:12:02 +0100

    parce textures using one function instead of 4 functions

--- primary/parse_textures.c
+++ primary/parse_textures.c
@@ -6,90 +6,30 @@
 /*   By: moboustt <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/21 14:18:42 by moboustt          #+#    #+#             */
-/*   Updated: 2020/11/26 11:11:55 by moboustt         ###   ########.fr       */
+/*   Updated: 2020/11/28 10:46:58 by moboustt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3d.h"
 
-void	get_western_texture_path(t_struct *data, const char *buff)
+void	get_texture_path(t_struct *data, const char *buff, int found, char *txt)
 {
-	int i;
+    int i;
 
-	i = 0;
-	if (data->found_we)
-		error("\e[0;31m duplicate west texture symbol\n");
-	data->pos += 2;
-	if (!is_valid_texture(data, buff))
-		error("\e[0;31m Invalid texture path!\n");
-	while (buff[(data->pos)] != '\n')
-	{
-		data->we[i] = buff[(data->pos)];
-		i++;
-		(data->pos)++;
-	}
-	data->get_to_map += 1;
-	data->found_we = 1;
-}
-
-void	get_easter_texture_path(t_struct *data, const char *buff)
-{
-	int i;
-
-	i = 0;
-	if (data->found_ea)
-		error("\e[0;31m duplicate east texture symbol\n");
-	data->pos += 2;
-	if (!is_valid_texture(data, buff))
-		error("\e[0;31m Invalid texture path!\n");
-	while (buff[(data->pos)] != '\n')
-	{
-		data->ea[i] = buff[(data->pos)];
-		i++;
-		(data->pos)++;
-	}
-	data->get_to_map += 1;
-	data->found_ea = 1;
-}
-
-void	get_north_texture_path(t_struct *data, const char *buff)
-{
-	int i;
-
-	i = 0;
-	if (data->found_no)
-		error("\e[0;31m duplicate north texture symbol\n");
-	data->pos += 2;
-	if (!is_valid_texture(data, buff))
-		error("Invalid texture path!\n");
-	while (buff[(data->pos)] != '\n')
-	{
-		data->no[i] = buff[data->pos];
-		i++;
-		data->pos++;
-	}
-	data->get_to_map += 1;
-	data->found_no = 1;
-}
-
-void	get_south_texture_path(t_struct *data, const char *buff)
-{
-	int i;
-
-	i = 0;
-	if (data->found_so)
-		error("\e[0;31m duplicate south texture symbol\n");
-	data->pos += 2;
-	if (!is_valid_texture(data, buff))
-		error("\e[0;31m Invalid texture path!\n");
-	while (buff[(data->pos)] != '\n')
-	{
-		data->so[i] = buff[data->pos];
-		i++;
-		data->pos++;
-	}
-	data->get_to_map += 1;
-	data->found_so = 1;
+    i = 0;
+    if (found)
+        error("\e[0;31m duplicate west texture symbol\n");
+    data->pos += 2;
+    if (!is_valid_texture(data, buff))
+        error("\e[0;31m Invalid texture path!\n");
+    while (buff[(data->pos)] != '\n')
+    {
+        txt[i] = buff[(data->pos)];
+        i++;
+        (data->pos)++;
+    }
+    data->get_to_map += 1;
+    found = 1;
 }
 
 void	get_sprite_path(t_struct *data, char *buff)
